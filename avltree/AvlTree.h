b9af33d16eac1a5cb087975eb8e6714fe02b7a36
jo-code <jan@ottisolutions.de>
2017-12-11 14:13:30 +0100

    basic .h file

--- avltree/AvlTree.h
+++ avltree/AvlTree.h
@@ -1,8 +1,27 @@
-//
-// Created by flori on 11.12.2017.
-//
-
 #ifndef AVLTREE_AVLTREE_H
 #define AVLTREE_AVLTREE_H
+
+class AvlTree {
+
+private:
+
+    struct Node {
+        const int key;
+        Node *left = nullptr;
+        Node *right = nullptr;
+        Node(const int);
+        Node(const int, Node *, Node *);
+        ~Node();
+    };
+
+    Node *root = nullptr;
+
+public:
+    ~AvlTree();
+    bool search(const int) const;
+    void insert(const int);
+    void remove(const int);
+};
+
 
 #endif //AVLTREE_AVLTREE_H
