eba1b944013e183ccbf37e3a9cbfbad3f0bf5d70
fo-code <orendi@hm.edu>
2017-12-11 14:25:14 +0100

    Methodenköpfe, Destruktoren und Konstruktoren hinzugefügt

--- avltree/AvlTree.cpp
+++ avltree/AvlTree.cpp
@@ -1,1 +1,33 @@
 #include "AvlTree.h"
+
+
+/********************************************************************
+ * Konstruktoren
+ ********************************************************************/
+AvlTree::Node::Node(const int k) : key(k) {};
+
+AvlTree::Node::Node(const int k, Node* l, Node* r) : key(k), left(l), right(r) {};
+
+
+/********************************************************************
+* Destruktoren
+*********************************************************************/
+AvlTree::~AvlTree() {
+    delete root;
+}
+AvlTree::Node::~Node() {
+    delete left;
+    delete right;
+}
+
+
+void AvlTree::insert(const int key) {
+
+}
+void AvlTree::remove(const int key) {
+
+}
+
+bool AvlTree::search(const int key) const {
+    return true;
+}
